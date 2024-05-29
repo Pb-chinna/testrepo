86e28946076128bad8e46a31c9a590dc0201a9af
Pb-chinna <chinnapb09@gmail.com>
2024-05-29 21:09:41 +0530

    add constructor

--- student/src/main/java/com/example/controller/studentcontroller.java
+++ student/src/main/java/com/example/controller/studentcontroller.java
@@ -4,5 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class studentcontroller {
-
+	public studentcontroller() {
+		System.out.println("welcome to studentcontroller");
+	}
 }
